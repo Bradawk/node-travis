d3464dd88ebaf63645f6d00d1a188470502f3534
Brad <niepceron.brad@gmail.com>
2021-01-21 15:11:01 +0100

    cul

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const app = express();
 
 app.get('/', (req, res) => {
-  res.send('INCROYABLE DU CUL.');
+  res.send('INCROYABLE DES CULS.');
 });
 
 const server = app.listen(8080, () => {

0c50040741aea8f86f5b879c2f359ec288a08816
Brad <niepceron.brad@gmail.com>
2021-01-21 14:27:15 +0100

    Test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,12 +12,12 @@ gcloud --quiet components install kubectl
 
 echo ${GCLOUD_SERVICE_KEY} | base64 --decode -i > ${HOME}/gcloud-service-key.json
 gcloud auth activate-service-account --key-file ${HOME}/gcloud-service-key.json
-
 gcloud --quiet config set project ${PROJECT_ID}
 gcloud --quiet config set container/cluster ${CLUSTER_NAME}
 gcloud --quiet config set compute/zone ${CLOUDSDK_COMPUTE_ZONE}
 gcloud --quiet container clusters get-credentials ${CLUSTER_NAME}
 
+gcloud auth configure-docker
 docker build -t ${APP_NAME} .
 docker tag ${APP_NAME} gcr.io/${PROJECT_ID}/${APP_NAME}:${TRAVIS_COMMIT}
 gcloud docker -- push gcr.io/${PROJECT_ID}/${APP_NAME}:${TRAVIS_COMMIT}
